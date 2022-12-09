977b2d7c17f92ce5510d7302a391f61b31cfb09f
tressa <109493624+TressaDanvers@users.noreply.github.com>
2022-12-09 10:27:21 -0500

    added installer

---
+++ installer/install cassert.sh
@@ -0,0 +1,3 @@
+sudo cp ./assertions.h /usr/include
+sudo cp ./libassert.so /usr/lib
+sudo /sbin/ldconfig
---
+++ pack installer.sh
@@ -0,0 +1,17 @@
+rm ./installer.zip
+rm -fr ./installer
+mkdir ./installer
+
+# CTest Installer
+cp ./build/ctest ./installer/
+echo sudo cp ./ctest /bin/> ./installer/install\ ctest.sh
+
+# CAssert Installer
+cp ./source/assertions.h ./installer/
+cp ./build/libassert.so ./installer/
+echo sudo cp ./assertions.h /usr/include > ./installer/install\ cassert.sh
+echo sudo cp ./libassert.so /usr/lib >> ./installer/install\ cassert.sh
+echo sudo /sbin/ldconfig >> ./installer/install\ cassert.sh
+
+# Zip
+zip -r ./installer.zip ./installer
